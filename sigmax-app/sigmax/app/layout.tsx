3b714ceda549093c45edee27ba7b79120184aa7a
jurgenpavel <jurgenpavelalexis@gmail.com>
2025-10-07 16:29:35 -0600

    Update layout.tsx

--- sigmax-app/sigmax/app/layout.tsx
+++ sigmax-app/sigmax/app/layout.tsx
@@ -1,10 +1,12 @@
-
 import "./../styles/globals.css";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "SIGMAX — Calculadora de Capacidad de Proceso",
   description: "Calculadora Six Sigma / Capacidad de Proceso",
+  icons: {
+    icon: "/icon.png", // ← aquí se enlaza tu imagen
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
